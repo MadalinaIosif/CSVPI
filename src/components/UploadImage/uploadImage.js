972924fa17a51982ca57859473d5072e97638680
Ovejdanie Gabriel <gabriel.ovejdanie@gmail.com>
2021-11-15 17:31:20 +0200

    Change to arrow function Restyling

--- src/components/UploadImage/uploadImage.js
+++ src/components/UploadImage/uploadImage.js
@@ -1,47 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./uploadImage.css"
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
 
 const options = [
-  'one', 'two', 'three'
+  'onasdasdsdasdasdas a asd asdasdasde', 'two', 'three'
 ];
 const defaultOption = options[0];
 
-export default class UploadImages extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.selectFile = this.selectFile.bind(this);
+const UploadImages = () => {
+  const [previewImage, setPreviewImage] = useState();
 
-    this.state = {
-      previewImage: undefined,
-    };
+
+  const selectFile = (event) => {
+    setPreviewImage( URL.createObjectURL(event.target.files[0]));
   }
-
-  onSelect = e => {
-      console.log(e)
-  }  
-
-  selectFile(event) {
-    this.setState({
-      previewImage: URL.createObjectURL(event.target.files[0]),
-    });
-  }
-
-  render() {
-    const { previewImage } = this.state;
-
-    return (
-      <div>
+  return (
+    <div>
         <div>
           <div className="main-upload">
             <label className="upload-btn">
-              <input id="actual-btn" type="file" accept="image/*" onChange={this.selectFile} hidden/>
+              <input id="actual-btn" type="file" accept="image/*" onChange={selectFile} hidden/>
               <label className="upload-button" htmlFor="actual-btn">Upload Image</label>
             </label>
-            <Dropdown options={options} onChange={this.onSelect} value={defaultOption} placeholder="Select an option" />
+            <Dropdown className="dropdown" options={options} onChange={this.onSelect} value={defaultOption} placeholder="Select an option" />
 
           </div>
         </div>
@@ -52,6 +35,7 @@ export default class UploadImages extends Component {
           </div>
         )}
       </div>
-    );
-  }
+  )
 }
+
+export default UploadImages;
